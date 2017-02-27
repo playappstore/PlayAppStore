02b2b6b8e1c5975420936c331e48c4bcec5dec2d
kongdeqin <kongdeqin@cheyipai.com>
2017-02-27 14:54:29 +0800

    发现页面

---
+++ iOS/PlayAppStore/SubProject/DiscoveSystem/Controllers/PASDescoverListViewController.m
@@ -0,0 +1,37 @@
+//
+//  PASDescoverListViewController.m
+//  PlayAppStore
+//
+//  Created by cheyipai.com on 2017/2/27.
+//  Copyright © 2017年 Winn. All rights reserved.
+//
+
+#import "PASDescoverListViewController.h"
+
+@interface PASDescoverListViewController ()
+
+@end
+
+@implementation PASDescoverListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ iOS/PlayAppStore/SubProject/DiscoveSystem/Controllers/PASDiscoverController.h
@@ -0,0 +1,13 @@
+//
+//  PASFindController.h
+//  PlayAppStore
+//
+//  Created by Winn on 2017/2/18.
+//  Copyright © 2017年 Winn. All rights reserved.
+//
+
+#import "PASViewController.h"
+
+@interface PASDiscoverController : PASViewController
+
+@end
