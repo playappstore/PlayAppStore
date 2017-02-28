ee4e5ba6b0e5d5caa0c0246e709747188e521925
kongdeqin <kongdeqin@cheyipai.com>
2017-02-28 17:44:00 +0800

    新建文件夹，关注

--- iOS/PlayAppStore/SubProject/Follow/VC/PASFollowController.m
+++ iOS/PlayAppStore/SubProject/Follow/VC/PASFollowController.m
@@ -7,17 +7,77 @@
 //
 
 #import "PASFollowController.h"
+#import "PASDisListTableViewCell.h"
+#import "PASApplicationDetailController.h"
+NSString * const cellRes1 = @"PASDisListTableViewCell1";
+@interface PASFollowController ()<UITableViewDataSource,UITableViewDelegate>{
+    
+    UITableView *_followTableView;
 
-@interface PASFollowController ()
+}
 
 @end
 
 @implementation PASFollowController
-
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = NO;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initData];
+    [self initView];
+    
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor greenColor];}
+}
+- (void)initData {
+    
+}
+- (void)initView {
+    
+    [self initTableView];
+}
+- (void)initTableView {
+    
+    _followTableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    _followTableView.delegate = self;
+    _followTableView.dataSource = self;
+    _followTableView.rowHeight = PASDisListTableViewCellHeight;
+    _followTableView.sectionHeaderHeight = 40;
+    [_followTableView registerClass:[PASDisListTableViewCell class] forCellReuseIdentifier:cellRes1];
+    [self.view addSubview:_followTableView];
+}
+#pragma mark -- tableViewDelegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 3;
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    return 2;
+
+}
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+
+    return [UIView new];
+
+}
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+
+    return @"应用名称a";
+
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    PASDisListTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellRes1];
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    PASApplicationDetailController *detailController = [[PASApplicationDetailController alloc] init];
+    [self.navigationController pushViewController:detailController animated:YES];
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
