9fff8bd038e4529b4b3e8132b55d8e5439a84771
kongdeqin <kongdeqin@cheyipai.com>
2017-03-01 11:11:38 +0800

    关注页面创建Cell

---
+++ iOS/PlayAppStore/SubProject/Follow/View/PASFollowTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  PASFollowTableViewCell.m
+//  PlayAppStore
+//
+//  Created by cheyipai.com on 2017/3/1.
+//  Copyright © 2017年 Winn. All rights reserved.
+//
+
+#import "PASFollowTableViewCell.h"
+
+@implementation PASFollowTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
