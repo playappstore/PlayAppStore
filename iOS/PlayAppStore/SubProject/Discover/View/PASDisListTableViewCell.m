059bc1feba52601c103f976aef8f959000c64591
kong <dearkong@gmail.com>
2017-03-01 22:26:36 +0800

    cell约束

--- iOS/PlayAppStore/SubProject/Discover/View/PASDisListTableViewCell.m
+++ iOS/PlayAppStore/SubProject/Discover/View/PASDisListTableViewCell.m
@@ -9,6 +9,8 @@
 #import "PASDisListTableViewCell.h"
 #import "UIImageView+CornerRadius.h"
 #import "UIColor+PKDownloadButton.h"
+#import <Masonry/Masonry.h>
+
 @implementation PASDisListTableViewCell
 
 - (void)awakeFromNib {
@@ -36,14 +38,16 @@
     [self.contentView addSubview:self.logoImageView];
     
     //更新时间
-    _upDataTimeLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.logoImageView.right + 20, self.logoImageView.top , SCREEN_WIDTH - (self.logoImageView.right + 20) , 15)];
-    _upDataTimeLabel.text = @"更新时间：2017.02.28 10:10";
+    _upDataTimeLabel = [[UILabel alloc] init];
+//                        WithFrame:CGRectMake(self.logoImageView.right + 20, self.logoImageView.top , SCREEN_WIDTH - (self.logoImageView.right + 20) , 15)];
+    _upDataTimeLabel.text = @"更新时间：2017.02.28 10:10 2234444444";
     _upDataTimeLabel.font = [UIFont systemFontOfSize:13];
     _upDataTimeLabel.textColor = RGBCodeColor(0x666666);
     [self.contentView addSubview:_upDataTimeLabel];
     
     //版本
-    _versionsLabel = [[UILabel alloc] initWithFrame:CGRectMake(_upDataTimeLabel.left, _upDataTimeLabel.bottom + 10, _upDataTimeLabel.width - 40, _upDataTimeLabel.height)];
+    _versionsLabel = [[UILabel alloc] init];
+//                      .WithFrame:CGRectMake(_upDataTimeLabel.left, _upDataTimeLabel.bottom + 10, _upDataTimeLabel.width - 40, _upDataTimeLabel.height)];
     _versionsLabel.textColor = _upDataTimeLabel.textColor;
     _versionsLabel.font = _upDataTimeLabel.font;
     _versionsLabel.text = @"版本：5.0.1";
@@ -51,7 +55,7 @@
     
 
     //下载按钮
-    _downloadButton = [[PKDownloadButton alloc] initWithFrame:CGRectMake(SCREEN_WIDTH - 110, _upDataTimeLabel.bottom , 100, 30)];
+    _downloadButton = [[PKDownloadButton alloc] init];
     _downloadButton.backgroundColor = [UIColor clearColor];
     [self.downloadButton.downloadedButton cleanDefaultAppearance];
     [self.downloadButton.downloadedButton setTitle:@"h" forState:UIControlStateNormal];
@@ -61,6 +65,7 @@
     self.downloadButton.stopDownloadButton.tintColor = [UIColor blackColor];
     self.downloadButton.stopDownloadButton.filledLineStyleOuter = YES;
     NSAttributedString *title = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@",NSLocalizedString(@"DOWNLOAD", nil)] attributes:@{ NSForegroundColorAttributeName : [UIColor defaultDwonloadButtonBlueColor],NSFontAttributeName : [UIFont systemFontOfSize:14.f]}];
+    self.downloadButton.startDownloadButton.contentEdgeInsets = UIEdgeInsetsMake(0, 5, 0, 5);
     [self.downloadButton.startDownloadButton setAttributedTitle:title forState:UIControlStateNormal];
     self.downloadButton.pendingView.tintColor = [UIColor defaultDwonloadButtonBlueColor];
     self.downloadButton.stopDownloadButton.tintColor = [UIColor defaultDwonloadButtonBlueColor];
@@ -70,15 +75,37 @@
     self.downloadButton.delegate = self;
     [self.contentView addSubview:_downloadButton];
     
-    
-
     //更新能容
     _describeLabel = [[UILabel alloc] initWithFrame:CGRectMake(_upDataTimeLabel.left, self.downloadButton.bottom + 5, SCREEN_WIDTH -_upDataTimeLabel.left - 10 , PASDisListTableViewCellHeight -self.downloadButton.bottom - 10 )];
     _describeLabel.textColor = _upDataTimeLabel.textColor;
     _describeLabel.font = _upDataTimeLabel.font;
-    _describeLabel.text = @"这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容";
+//    _describeLabel.text = @"这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容这是更新内容";
     _describeLabel.numberOfLines = 0;
     [self.contentView addSubview:_describeLabel];
+//    [self.describeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(-14);
+//        make.height.mas_equalTo(30);
+//        make.centerY.equalTo(self.contentView);
+//    }];
+    [self PAS_mas_makeConstraints];
+
+
+}
+- (void)PAS_mas_makeConstraints {
+
+    [self.downloadButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.offset(-14);
+        make.height.mas_equalTo(30);
+        make.centerY.equalTo(self.logoImageView);
+    }];
+    [self.upDataTimeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(self.downloadButton.mas_left).offset(- 10).priorityLow();
+        make.top.equalTo(self.logoImageView.mas_top);
+        make.left.equalTo(self.logoImageView.mas_right).offset(20);
+    }];
+
+    
+
 
 }
 - (void)downloadButtonTapped:(PKDownloadButton *)downloadButton
