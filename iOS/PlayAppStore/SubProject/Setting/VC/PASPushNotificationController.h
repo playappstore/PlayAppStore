d770e8e537ec6e6d458b8bc4942d4d7c30c3541a
hewei48 <hewei48@wanda.cn>
2017-03-01 15:31:51 +0800

    [add]add Setting details entrance of notification, share etc

---
+++ iOS/PlayAppStore/SubProject/Setting/VC/PASPushNotificationController.h
@@ -0,0 +1,13 @@
+//
+//  PASPushNotificationController.h
+//  PlayAppStore
+//
+//  Created by Winn on 2017/3/1.
+//  Copyright © 2017年 Winn. All rights reserved.
+//
+
+#import "PASViewController.h"
+
+@interface PASPushNotificationController : PASViewController
+
+@end
---
+++ iOS/PlayAppStore/SubProject/Setting/View/PASChangeLanguageCell.h
@@ -0,0 +1,15 @@
+//
+//  PASChangeLanguageCell.h
+//  PlayAppStore
+//
+//  Created by Winn on 2017/3/1.
+//  Copyright © 2017年 Winn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PASChangeLanguageCell : UITableViewCell
+
++ (instancetype)cellCreatedWithTableView:(UITableView *)tableView;
+
+@end
